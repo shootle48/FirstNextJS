ea71b57afc4b40471977fc559c707e7440bb8259
shootle48 <theprit.1316s@gmail.com>
2024-08-30 17:46:40 +0700

    up to git

--- src/app/aboutme/page.tsx
+++ src/app/aboutme/page.tsx
@@ -3,9 +3,9 @@ export default function AboutMePage() {
   return (
     <div>
       <h1>About Me</h1>
-      <p>Name: [Your Name]</p>
-      <p>Student ID: [Your Student ID]</p>
-      <p>University: [Your University]</p>
+      <p>Name: Thepparit Inthapraphan</p>
+      <p>Student ID: 653450091-4</p>
+      <p>CS</p>
     </div>
   );
 }
